a11bc1f75d5510edeb310ffd2e819948404e69ec
Warodom Werapun <warodom.w@psu.ac.th>
2024-02-21 16:06:19 +0700

    Init commit

--- create.ps1
+++
@@ -1,6 +0,0 @@
-# Create 10 files with .sol extension
-for ($i = 1; $i -le 24; $i++) {
-    $fileName = ".\Chapter4\SourceCode4_$i.sol"
-    New-Item -Path $fileName -ItemType File
-    Write-Host "Created file: $fileName"
-}
