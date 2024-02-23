465f4f41daa818fd3f7e1b5a2d9f262c6063af1f
Warodom Werapun <wwarodom@gmail.com>
2024-02-23 08:06:47 +0700

    Update SourceCode4_13.sol

--- Chapter4/SourceCode4_13.sol
+++ Chapter4/SourceCode4_13.sol
@@ -1,3 +1,34 @@
-function doSomething() public onlyOwner {
-    // Function code goes here
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Example {    
+
+    address private owner;
+    uint balance;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _; // This underscore represents the original function's code
+    }
+
+    modifier balanceNotZero {
+        // Modifier 2 code
+        _;
+        require( balance > 0 ,"Balance must greater than 0" );
+    }
+
+    constructor() {
+        balance = 0;
+        owner = msg.sender;
+    }
+
+    
+    function doSomething() public view onlyOwner returns (string memory) {
+        return "Hello";
+    }
+
+    function doSomething2(uint _balance) public onlyOwner balanceNotZero returns (uint) {
+        balance = _balance;
+        return balance;
+    }
 }
