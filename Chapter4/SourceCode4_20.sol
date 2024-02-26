ebd767e06ee362078d3189bada0d52a60cb168ae
Warodom Werapun <wwarodom@gmail.com>
2024-02-26 11:37:02 +0700

    Update SourceCode4_20.sol

--- Chapter4/SourceCode4_20.sol
+++ Chapter4/SourceCode4_20.sol
@@ -18,13 +18,16 @@ contract ReceiveEther {
 }
 
 contract SendEther { 
-    function sendViaTransfer(address payable _to) public payable {
+    function sendViaTransfer(address payable _to) public payable { 
+        // work only transfer to EOA account, doesn't work for a smart contract address
+        // Failure to transfer to a smart contract address
+        // since it will execute receive() or fallback() that costs gas more 2300
         _to.transfer(msg.value);
     }
 
     function sendViaSend(address payable _to) public payable {
-        // Send returns a boolean value indicating success or failure.
-        // This function is not recommended for sending Ether.
+        // Similar to transfer() but returns a boolean value indicating success or failure.
+        // and also have default gas limit: 2300  as transfer()
         bool sent = _to.send(msg.value);
         require(sent, "Failed to send Ether");
     }
@@ -33,13 +36,14 @@ contract SendEther {
         // Call returns a boolean value indicating success or failure.
         // This is the current recommended method to use.
         // receive()  get called
-        (bool sent, ) = _to.call{value: msg.value}(""); 
+        // (bool sent, ) = _to.call{value: msg.value}(""); 
+        (bool sent, bytes memory data) = _to.call{gas :10000, value: msg.value}("");  // receive
         require(sent, "Failed to send Ether");
     }
     
     function sendViaCallWithData(address payable _to) public payable { 
         // fallback() get called, "withData" is sent via msg.data 
-        (bool sent, ) = _to.call{value: msg.value}("withData"); 
+        (bool sent, ) = _to.call{value: msg.value}("withData");                     // fallback
         require(sent, "Failed to send Ether");
     }
 }
