b6b13b57450e371c26f7d25c27752d9bda39de7b
Warodom Werapun <wwarodom@gmail.com>
2024-02-26 10:09:12 +0700

    Update SourceCode4_23.sol

--- Chapter4/SourceCode4_23.sol
+++ Chapter4/SourceCode4_23.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Parent {
-  event Log(string );
+contract Parent { 
+  event Log(string, uint, uint );
   string internal str = "Hello";  // public internal private (no external)
   // for visibility: private internal, external, public
   function foo()  internal pure returns (uint) { return 1; }
@@ -10,16 +10,16 @@ contract Parent {
   function bar() virtual  public  {
       foo(); // cannot execute with external
       foo(3);
-      emit Log("Parent");
+      emit Log("Parent", foo(), foo(3));
   }
 }
 contract Child is Parent {
-  Parent p = new Parent();
-  // Child ch = new Child();  // cannot instantiate
+  // Parent p = new Parent();
+  Child p = new Child();  // cannot instantiate
 
   function bar() override  public {
-      // foo();    // doesn't work with internal,internal work when override
-      emit Log("Child");
+      // foo();    // doesn't work with internal,internal work when override 
+      emit Log("Child",0,0);
   }
 
   function baz() public {
