d664449a3b7584bdd1ee929fe9a06a6a01b602aa
Warodom Werapun <wwarodom@gmail.com>
2024-03-01 10:22:20 +0700

    Update SourceCode4_21.sol

--- Chapter4/SourceCode4_21.sol
+++ Chapter4/SourceCode4_21.sol
@@ -3,14 +3,14 @@ pragma solidity 0.8.20;
 
 contract Teacher {
    string name;
-   constructor(string memory _name)  public {
+   constructor(string memory _name)  {
        name = _name;
    }
 }
 
 contract ComputerTeacher is Teacher {
    string department;
-   constructor(string memory _name, string memory _department)  Teacher(_name) public {
+   constructor(string memory _name, string memory _department)  Teacher(_name)  {
        department = _department;
    }
    function showDetails() public view returns (string memory, string memory) {
