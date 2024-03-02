cccb9765ef3072c8b08323e2f378f31f81d48677
Warodom Werapun <wwarodom@gmail.com>
2024-03-02 16:09:32 +0700

    Update SourceCode6_4.sol

--- Chapter6/SourceCode6_4.sol
+++ Chapter6/SourceCode6_4.sol
@@ -1,29 +1,23 @@
-// This contract is vulnerable and interacts with an external contract
-// without realizing the potential malicious behavior.
-
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20; 
 
-interface ExternalContract {
-    function transfer(address to, uint256 amount) external;
+pragma solidity 0.8.20;
+
+contract Innocent {
+    Mask mask; 
+
+    constructor(address _mask) {
+        mask = Mask(_mask);
+    } 
+
+    function callMaskLog() public {
+        mask.log();
+    }    
 }
 
-contract VictimContract {
-    ExternalContract externalContract;
-    uint256 public balance;
+contract Mask {
+    event Log(string message); 
 
-    constructor(address _externalContractAddress) {
-        externalContract = ExternalContract(_externalContractAddress);
-    }
-
-
-    function deposit() public payable {
-        balance += msg.value;
-    }
-
-    function withdraw() public {
-        uint256 amountToWithdraw = balance;
-        balance = 0;
-        externalContract.transfer(msg.sender, amountToWithdraw); // External call to transfer funds
+    function log() public  {
+        emit Log("Mask log was called");
     }
 }
