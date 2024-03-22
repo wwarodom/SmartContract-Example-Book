dd807eaf150f0fbbb146ff89590c2eef704111d5
Warodom Werapun <wwarodom@gmail.com>
2024-03-22 08:19:52 +0700

    Update SourceCode5_9.sol

--- Chapter5/SourceCode5_9.sol
+++ Chapter5/SourceCode5_9.sol
@@ -2,12 +2,12 @@
 
 pragma solidity 0.8.20;
 
-contract Uniswap {
+contract Uniswap01 {
     mapping(address => uint256) public balances;
 
     function swapTokens(uint256 amountIn, uint256 amountOut) public {
         // Simulate the token swap by deducting 'amountIn' tokens from the sender and adding 'amountOut' tokens to their balance.
-        balances[msg.sender] -= amountIn;
         balances[msg.sender] += amountOut;
+        balances[msg.sender] -= amountIn;
     }
 }
