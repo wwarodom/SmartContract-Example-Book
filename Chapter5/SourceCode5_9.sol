b8d4ef029640be13e95af55fed835953c8e27328
Warodom Werapun <wwarodom@gmail.com>
2024-03-26 14:49:57 +0700

    Update SourceCode5_9.sol

--- Chapter5/SourceCode5_9.sol
+++ Chapter5/SourceCode5_9.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.20;
 
 contract Uniswap01 {
-    mapping(address => uint256) public balances;
-
-    function swapTokens(uint256 amountIn, uint256 amountOut) public {
-        // Simulate the token swap by deducting 'amountIn' tokens from the sender and adding 'amountOut' tokens to their balance.
-        balances[msg.sender] += amountOut;
-        balances[msg.sender] -= amountIn;
+    mapping(address => mapping( string => uint256)) public balances;
+    constructor () {
+        balances[msg.sender]["ETH"] =  10 ether;
+        balances[msg.sender]["USDT"] = 3000 ether;
+    }
+    function swapTokens(uint256 amountIn, uint256 amountOut) public {        
+        balances[msg.sender]["ETH"] -= amountIn;
+        balances[msg.sender]["USDT"] += amountOut;
     }
 }
